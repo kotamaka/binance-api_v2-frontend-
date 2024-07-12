e9dc3f9c7e2b800b9804533904afaeb7de083070
kotamaka <43109370+kotamaka@users.noreply.github.com>
2024-07-12 05:25:11 +0000

    fix focus

--- main.py
+++ main.py
@@ -20,38 +20,42 @@ def historyPage():
             balance -= d['profit']/d['lot']
     else:
         st.error(f'Error code : {r.status_code}', icon="🚨")
-    text = '{:.2f}'.format(balance)
-    st.markdown(f'## Balance : {text}')
-    df = pd.DataFrame(data)
-    # Chart     
-    df['create_date'] = pd.to_datetime(df['create_date'])
-    df['adjusted_profit'] = df['profit'] * -1
-    chart_data = df[['create_date','adjusted_profit']]
-    chart_data = chart_data.rename(columns={"create_date": "Date", "adjusted_profit": "Profit"})
-    chart = alt.Chart(chart_data).mark_bar().encode(
-        x='Date:T',
-        y='Profit:Q',
-        color=alt.condition(
-            alt.datum.Profit > 0,
-            alt.value('green'),
-            alt.value('red')
+    
+    if 'message' in data:
+        st.warning('{}'.format(data['message']), icon="🚨")
+    else:
+        text = '{:.2f}'.format(balance)
+        st.markdown(f'## Balance : {text}')
+        df = pd.DataFrame(data)
+        # Chart     
+        df['create_date'] = pd.to_datetime(df['create_date'])
+        df['adjusted_profit'] = df['profit'] * -1
+        chart_data = df[['create_date','adjusted_profit']]
+        chart_data = chart_data.rename(columns={"create_date": "Date", "adjusted_profit": "Profit"})
+        chart = alt.Chart(chart_data).mark_bar().encode(
+            x='Date:T',
+            y='Profit:Q',
+            color=alt.condition(
+                alt.datum.Profit > 0,
+                alt.value('green'),
+                alt.value('red')
+            )
+        ).properties(
+            width=800,
+            height=400
+        ).configure_axis(
+            labelFontSize=12,
+            titleFontSize=14
         )
-    ).properties(
-        width=800,
-        height=400
-    ).configure_axis(
-        labelFontSize=12,
-        titleFontSize=14
-    )
-    st.markdown('<div class="custom-padding"></div>', unsafe_allow_html=True)
-    st.altair_chart(chart,use_container_width=True)
+        st.markdown('<div class="custom-padding"></div>', unsafe_allow_html=True)
+        st.altair_chart(chart,use_container_width=True)
 
-    # DataFrame
-    st.markdown('')
-    df = df.drop(columns=['id','adjusted_profit'])
-    df.columns = ['Date','Symbol','Entry Price','Market Price','Profit','Lot']
-    df = df.reindex(columns=['Symbol','Entry Price','Market Price','Profit','Lot','Date'])
-    st.dataframe(df, height=800,width=800,on_select="ignore")
+        # DataFrame
+        st.markdown('')
+        df = df.drop(columns=['id','adjusted_profit'])
+        df.columns = ['Date','Symbol','Entry Price','Market Price','Profit','Lot']
+        df = df.reindex(columns=['Symbol','Entry Price','Market Price','Profit','Lot','Date'])
+        st.dataframe(df, height=800,width=800,on_select="ignore")
 
 def focusPage():
     st.title('Focus')
@@ -63,11 +67,12 @@ def focusPage():
     else:
         st.error(f'Error code : {r.status_code}', icon="🚨")
 
-    df = pd.DataFrame(data)
-    df = df.drop(columns=['id'])
-    df.columns = ['Symbol','Change(%)']
-    st.dataframe(df,width=800,on_select="ignore")
-
+    if 'message' in data:
+        st.warning('{}'.format(data['message']), icon="🚨")
+    else:
+        df = df.drop(columns=['id'])
+        df.columns = ['Symbol','Change(%)']
+        st.dataframe(df,width=800,on_select="ignore")
 
 def ordersPage():
     st.title('Orders')
@@ -79,11 +84,14 @@ def ordersPage():
     else:
         st.error(f'Error code : {r.status_code}', icon="🚨")
     
-    df = pd.DataFrame(data)
-    df = df.drop(columns=['id'])
-    df.columns = ['Symbol','Type','Entry Price','Stoploss','Takeprofit']
-    st.dataframe(df,width=800,on_select="ignore")
-
+    if 'message' in data:
+        st.warning('{}'.format(data['message']), icon="🚨")
+    else:
+        df = pd.DataFrame(data)
+        df = df.drop(columns=['id'])
+        df.columns = ['Symbol','Type','Entry Price','Stoploss','Takeprofit']
+        st.dataframe(df,width=800,on_select="ignore")
+    
 st.sidebar.title('Aximorph API')
 page = st.sidebar.radio("Go to", ("Focus", "Orders", "History"))
 
